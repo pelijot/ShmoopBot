bf06e89950a3d71f18158cb0cde2fb292b497d32
pelijot <118746821+pelijot@users.noreply.github.com>
2026-04-07 17:28:24 +0200

    [M8B] Added Win Log

--- m8b.py
+++ m8b.py
@@ -2,11 +2,12 @@ import discord
 from dotenv import dotenv_values
 import random
 import os
+import time
 
 config = dotenv_values(".env")
 
 token = config.get("TOKEN")
-
+chance = int(config.get("WINCHANCE"))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -31,15 +32,20 @@ async def on_message(message):
 
     if client.user in message.mentions:
         lucky_num = random.randint(0,len(response_list) - 1)
-        win_game = random.randint(0,10000)
-        await message.reply(response_list[lucky_num])
-        print(f"Replied '{response_list[lucky_num]}' ({lucky_num}) to @{message.author} [{win_game / 100}]")
-        if win_game == 10000:
+        win_game = random.randint(0,chance)
+        if win_game == chance:
             await message.reply("You just won free access to the Steam (PC Version) of A Webbing Journey! Please contact <@701464203252203551>")
-
-
+            print(f"@{message.author} won [{win_game}%]")
+            with open("win.log", "a") as winlog:
+                winlog.write(f"{message.author} (ID: {message.author.id}) on {time.strftime('%a, %d %b %Y %H:%M:%S', time.localtime())}\n")
+            return
+        await message.reply(response_list[lucky_num])
+        print(f"Replied '{response_list[lucky_num]}' ({lucky_num}) to @{message.author} [{win_game}%]")
+       
 
 client.run("{}".format(token))
 
 
 # https://github.com/justinbaur/m8b/tree/master
+
+
