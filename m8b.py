1f6523f3d9e05adb0ad1d3cdd607a131aacdfdfb
pelijot <118746821+pelijot@users.noreply.github.com>
2026-04-07 16:59:16 +0200

    [M8B] Make bot respond to any form of pings

--- m8b.py
+++ m8b.py
@@ -29,7 +29,7 @@ async def on_message(message):
 #    if message.author == client.user:
 #        return
 
-    if message.content.startswith(f"<@{client.application_id}>"):
+    if client.user in message.mentions:
         lucky_num = random.randint(0,len(response_list) - 1)
         win_game = random.randint(0,10000)
         await message.reply(response_list[lucky_num])
